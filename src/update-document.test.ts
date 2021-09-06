89e28b64e2e8be6dd09025cf3b4fe78a1d585029
Artemnsk <shelkov1991@gmail.com>
2021-09-06 17:06:56 +0700

    update document tests

---
+++ src/update-document.test.ts
@@ -0,0 +1,75 @@
+import { client } from './client'
+
+const INDEX = 'test_index'
+
+async function searchDocs(query: string) {
+  const searchResult = await client.search({
+    index: INDEX,
+    body: {
+      query: {
+        multi_match: {
+          query,
+          fields: ['field1', 'field2', 'field3'],
+          type: 'phrase_prefix',
+        },
+      },
+    },
+  })
+  return searchResult.body.hits.hits
+}
+
+const DOCUMENT_ID = 'doc_id'
+const originalDocument = { field1: 'field1 something', field2: 'field2 something' }
+
+describe('Update API usage cases', () => {
+  afterEach(async () => {
+    const emptyCb = () => {}
+    await client.indices.delete({ index: INDEX }).catch(emptyCb)
+  })
+
+  beforeEach(async () => {
+    await client.indices.create({ index: INDEX })
+  })
+
+  it('Should update existing document', async () => {
+    await client.index({ index: INDEX, id: DOCUMENT_ID, body: originalDocument })
+
+    await client.update({
+      index: INDEX,
+      id: DOCUMENT_ID,
+      refresh: true,
+      body: { doc: { field2: 'field2 something 2', field3: 'field3 something' } },
+    })
+
+    const hits = await searchDocs('something')
+
+    expect(hits.length).toBe(1)
+    expect(hits[0]._source).toMatchObject({
+      field1: 'field1 something',
+      field2: 'field2 something 2',
+      field3: 'field3 something',
+    })
+  })
+
+  it('Should upsert document if doesnt exist', async () => {
+    const update = { field2: 'field2 something 2', field3: 'field3 something' }
+    await client.update({
+      index: INDEX,
+      id: DOCUMENT_ID,
+      refresh: true,
+      body: {
+        doc: update,
+        upsert: { ...originalDocument, ...update },
+      },
+    })
+
+    const hits = await searchDocs('something')
+
+    expect(hits.length).toBe(1)
+    expect(hits[0]._source).toMatchObject({
+      field1: 'field1 something',
+      field2: 'field2 something 2',
+      field3: 'field3 something',
+    })
+  })
+})
